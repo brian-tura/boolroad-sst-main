eb6ac3c785a000a4b04d17be73ccaac82341fded
brian-tura <b.tura2002@gmail.com>
2025-06-08 17:40:58 +0200

    [FIX] Layout fixes

--- src/pages/TripDetail.jsx
+++ src/pages/TripDetail.jsx
@@ -42,28 +42,32 @@ const TripDetail = () => {
 
     return (
         <>
-
-            <h4 className='tripTitle'>{`Partecipanti alla gita a ${trip.nome}`}</h4>
-            <div className="input-group">
-                <div className="form-outline d-flex" >
-                    <input type="text" placeholder='Cerca partecipante...' onChange={handleChange} value={searchInput} className="form-control border border-2 border-primary rounded-0 rounded-start" />
+            <div className='d-flex justify-content-between align-items-center mt-2'>
+                <div>
+                    <div className="input-group mb-2">
+                        <div className="form-outline d-flex" >
+                            <input type="text" placeholder='Cerca partecipante...' onChange={handleChange} value={searchInput} className="form-control border border-2 border-primary rounded-0 rounded-start" />
+                        </div>
+                    </div>
+                    <Link to={`/viaggi`}>
+                        <button
+                            type="button"
+                            className="btn btn-primary btn-rounded"
+                            data-mdb-ripple-init=""
+                        >
+                            Torna alla lista gite
+                        </button>
+                    </Link >
                 </div>
+                <h4 className='tripTitle'>{`Partecipanti alla gita a ${trip.nome}`}</h4>
             </div>
             <hr />
             {partecipantiFiltrati && partecipantiFiltrati.map((partecipante) => {
                 return (
                     <PartecipanteCard nome={partecipante.nome} cognome={partecipante.cognome} codice_fiscale={partecipante.codice_fiscale} telefono={partecipante.telefono} email={partecipante.email} index={partecipante.id} />
                 )
-            })}
-            <Link to={`/viaggi`}>
-                <button
-                    type="button"
-                    className="btn btn-primary btn-rounded"
-                    data-mdb-ripple-init=""
-                >
-                    Torna alla lista gite
-                </button>
-            </Link>
+            })
+            }
         </>
     )
 }
